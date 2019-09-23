75018b96b4c4b8a73f99807c49105421943144b5
Murilo Henrique <Muriloddd1@gmail.com>
2019-09-23 05:28:20 -0300

    Update main.cpp

--- escultor3d/main.cpp
+++ escultor3d/main.cpp
@@ -4,5 +4,26 @@
 using namespace std;
 
 int main(){
-    return 0;
+    #include <iostream>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include "sculptor.h"
+#include <cstring>
+using namespace std;
+
+int main(){
+ Sculptor g(20,20,20);
+ char *c = "texto.txt";
+
+ g.setColor(1,0.2,0.4,1);
+ g.putSphere(20/2,20/2,20/2,10);
+ g.putBox(0,1,0,1,0,1);
+ g.putEllipsoid(20/2,20/2,20/2,10,13,15);
+ g.cutSphere(20/2,20/2,20/2,10);
+ g.cutBox(0,1,0,1,0,1);
+ g.cutEllipsoid(20/2,20/2,20/2,10,13,15);
+
+ g.writeOFF(c);
+
 }
