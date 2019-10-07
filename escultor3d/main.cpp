bda5925b6c59bea091106ec9ddb39f23353a7669
BrenoXFD <brenoaraujole@gmail.com>
2019-10-07 07:36:39 -0300

    Teste

--- escultor3d/main.cpp
+++ escultor3d/main.cpp
@@ -7,17 +7,17 @@
 using namespace std;
 
 int main(){
- Sculptor g(20,20,20);
+    Sculptor g(20,20,20);
 
 
- g.setColor(1,0.2,0.4,1);
- g.putSphere(20/2,20/2,20/2,10);
- g.putBox(0,1,0,1,0,1);
- g.putEllipsoid(5,5,5,1,1,1);
- g.cutSphere(20/2,20/2,20/2,10);
- g.cutBox(0,1,0,1,0,1);
- g.cutEllipsoid(5,5,5,1,1,1);
+    g.setColor(1,0.2,0.4,1);
+    g.putSphere(20/2,20/2,20/2,10);
+    //g.putBox(0,1,0,1,0,1);
+    //g.putEllipsoid(5,5,5,1,1,1);
+    //g.cutSphere(20/2,20/2,20/2,10);
+    //g.cutBox(0,1,0,1,0,1);
+    //g.cutEllipsoid(5,5,5,1,1,1);
 
- g.writeOFF("C:/alo/test.txt");
- return 0;
+    g.writeOFF("C:/Users/UFRNLAB/Desktop/escultor3d/escultor3d/teste.off");
+    return 0;
 }
