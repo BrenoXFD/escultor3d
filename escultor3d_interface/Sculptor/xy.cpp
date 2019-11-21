b9312140d656bce23991da2f98d790b01dd94264
BrenoXFD <brenoaraujole@gmail.com>
2019-11-21 08:38:35 -0300

    Interface

--- escultor3d_interface/Sculptor/xy.cpp
+++ escultor3d_interface/Sculptor/xy.cpp
@@ -17,215 +17,68 @@ void Xy::paintEvent(QPaintEvent *event){
     pen.setWidth(height()/32);
 
     int tamanho = height()/32;
-    int xcenter, ycenter;
+    int xcenter, ycenter, x, y, aux;
     xcenter = width()/2;
     ycenter = height()/2;
+    aux = 0;
 
-    for(int i=0; i<16; i++){
-        int aux = 0;
-        for(int j=0; j<8; j++){
-            if(i%2==0){
-                int x1, y1, x2, y2;
-                x1 = (aux*tamanho) + xcenter;
-                y1 = (i*tamanho) + ycenter;
-                x2 = ((aux+1)*tamanho) + xcenter;
-                y2 = y1;
+    for(int i=0; i<32; i++){
+        for(int j=0; j<32; j++){
+            if(i<16 && j<16){
+                x = xcenter - (j*tamanho);
+                y = ycenter - (i*tamanho);
+                if(aux%2==0){
+                    pen.setColor(QColor(211,212,214));
+                    painter.setPen(pen);
+                    painter.drawPoint(x,y);
+                }else{
+                    pen.setColor(QColor(255,255,255));
+                    painter.setPen(pen);
+                    painter.drawPoint(x,y);
+                }
 
-                int x3, y3, x4, y4;
-                x3 = ((aux+1)*tamanho) + xcenter;
-                y3 = y1;
-                x4 = ((aux+1)*tamanho) + xcenter;
-                y4 = y1;
-
-                pen.setColor(QColor(211,212,214));
-                painter.setPen(pen);
-                painter.drawLine(x1,y1,x2,y2);
-
-                pen.setColor(QColor(255,255,255));
-                painter.setPen(pen);
-                painter.drawLine(x3,y3,x4,y4);
-
-                aux = aux + 2;
-            }else{
-                int x1, y1, x2, y2;
-                x1 = (aux*tamanho) + xcenter;
-                y1 = (i*tamanho) + ycenter;
-                x2 = ((aux+1)*tamanho) + xcenter;
-                y2 = y1;
-
-                int x3, y3, x4, y4;
-                x3 = ((aux+1)*tamanho) + xcenter;
-                y3 = y1;
-                x4 = ((aux+1)*tamanho) + xcenter;
-                y4 = y1;
-
-                pen.setColor(QColor(255,255,255));
-                painter.setPen(pen);
-                painter.drawLine(x1,y1,x2,y2);
-
-                pen.setColor(QColor(211,212,214));
-                painter.setPen(pen);
-                painter.drawLine(x3,y3,x4,y4);
-
-                aux = aux + 2;
             }
-        }
-    }
-
-    for(int i=0; i<16; i++){
-        int aux = 0;
-        for(int j=0; j<8; j++){
-            if(i%2==0){
-                int x1, y1, x2, y2;
-                x1 = (aux*tamanho) + xcenter;
-                y1 = ycenter - (tamanho*i);
-                x2 = ((aux+1)*tamanho) + xcenter;
-                y2 = y1;
-
-                int x3, y3, x4, y4;
-                x3 = ((aux+1)*tamanho) + xcenter;
-                y3 = y1;
-                x4 = ((aux+1)*tamanho) + xcenter;
-                y4 = y1;
-
-                pen.setColor(QColor(211,212,214));
-                painter.setPen(pen);
-                painter.drawLine(x1,y1,x2,y2);
-
-                pen.setColor(QColor(255,255,255));
-                painter.setPen(pen);
-                painter.drawLine(x3,y3,x4,y4);
-
-                aux = aux + 2;
-            }else{
-                int x1, y1, x2, y2;
-                x1 = (aux*tamanho) + xcenter;
-                y1 = ycenter - (tamanho*i);
-                x2 = ((aux+1)*tamanho) + xcenter;
-                y2 = y1;
-
-                int x3, y3, x4, y4;
-                x3 = ((aux+1)*tamanho) + xcenter;
-                y3 = y1;
-                x4 = ((aux+1)*tamanho) + xcenter;
-                y4 = y1;
-
-                pen.setColor(QColor(255,255,255));
-                painter.setPen(pen);
-                painter.drawLine(x1,y1,x2,y2);
-
-                pen.setColor(QColor(211,212,214));
-                painter.setPen(pen);
-                painter.drawLine(x3,y3,x4,y4);
-
-                aux = aux + 2;
+            if(i<16 && j>=16){
+                x = xcenter + ((j-15)*tamanho);
+                y = ycenter - (i*tamanho);
+                if(aux%2==0){
+                    pen.setColor(QColor(255,255,255));
+                    painter.setPen(pen);
+                    painter.drawPoint(x,y);
+                }else{
+                    pen.setColor(QColor(211,212,214));
+                    painter.setPen(pen);
+                    painter.drawPoint(x,y);
+                }
             }
-        }
-    }
-
-
-    for(int i=0; i<16; i++){
-        int aux = 0;
-        for(int j=0; j<8; j++){
-            if(i%2==0){
-                int x1, y1, x2, y2;
-                x1 = xcenter - (aux*tamanho);
-                y1 = ycenter - (tamanho*i);
-                x2 = xcenter - ((aux+1)*tamanho);
-                y2 = y1;
-
-                int x3, y3, x4, y4;
-                x3 = xcenter - ((aux+1)*tamanho);
-                y3 = y1;
-                x4 = xcenter - ((aux+1)*tamanho);
-                y4 = y1;
-
-                pen.setColor(QColor(211,212,214));
-                painter.setPen(pen);
-                painter.drawLine(x1,y1,x2,y2);
-
-                pen.setColor(QColor(255,255,255));
-                painter.setPen(pen);
-                painter.drawLine(x3,y3,x4,y4);
-
-                aux = aux + 2;
-            }else{
-                int x1, y1, x2, y2;
-                x1 = xcenter - (aux*tamanho);
-                y1 = ycenter - (tamanho*i);
-                x2 = xcenter - ((aux+1)*tamanho);
-                y2 = y1;
-
-                int x3, y3, x4, y4;
-                x3 = xcenter - ((aux+1)*tamanho);
-                y3 = y1;
-                x4 = xcenter - ((aux+1)*tamanho);
-                y4 = y1;
-
-                pen.setColor(QColor(255,255,255));
-                painter.setPen(pen);
-                painter.drawLine(x1,y1,x2,y2);
-
-                pen.setColor(QColor(211,212,214));
-                painter.setPen(pen);
-                painter.drawLine(x3,y3,x4,y4);
-
-                aux = aux + 2;
+            if(i>=16 && j<16){
+                x = xcenter - (j*tamanho);
+                y = ycenter + ((i-15)*tamanho);
+                if(aux%2==0){
+                    pen.setColor(QColor(255,255,255));
+                    painter.setPen(pen);
+                    painter.drawPoint(x,y);
+                }else{
+                    pen.setColor(QColor(211,212,214));
+                    painter.setPen(pen);
+                    painter.drawPoint(x,y);
+                }
             }
-        }
-    }
-
-    for(int i=0; i<16; i++){
-        int aux = 0;
-        for(int j=0; j<8; j++){
-            if(i%2==0){
-                int x1, y1, x2, y2;
-                x1 = xcenter - (aux*tamanho);
-                y1 = (i*tamanho) + ycenter;
-                x2 = xcenter - ((aux+1)*tamanho);
-                y2 = y1;
-
-                int x3, y3, x4, y4;
-                x3 = xcenter - ((aux+1)*tamanho);
-                y3 = y1;
-                x4 = xcenter - ((aux+1)*tamanho);
-                y4 = y1;
-
-                pen.setColor(QColor(211,212,214));
-                painter.setPen(pen);
-                painter.drawLine(x1,y1,x2,y2);
-
-                pen.setColor(QColor(255,255,255));
-                painter.setPen(pen);
-                painter.drawLine(x3,y3,x4,y4);
-
-                aux = aux + 2;
-            }else{
-                int x1, y1, x2, y2;
-                x1 = xcenter - (aux*tamanho);
-                y1 = (i*tamanho) + ycenter;
-                x2 = xcenter - ((aux+1)*tamanho);
-                y2 = y1;
-
-                int x3, y3, x4, y4;
-                x3 = xcenter - ((aux+1)*tamanho);
-                y3 = y1;
-                x4 = xcenter - ((aux+1)*tamanho);
-                y4 = y1;
-
-                pen.setColor(QColor(255,255,255));
-                painter.setPen(pen);
-                painter.drawLine(x1,y1,x2,y2);
-
-                pen.setColor(QColor(211,212,214));
-                painter.setPen(pen);
-                painter.drawLine(x3,y3,x4,y4);
-
-                aux = aux + 2;
+            if(i>=16 && j>=16){
+                x = xcenter + ((j-15)*tamanho);
+                y = ycenter + ((i-15)*tamanho);
+                if(aux%2==0){
+                    pen.setColor(QColor(211,212,214));
+                    painter.setPen(pen);
+                    painter.drawPoint(x,y);
+                }else{
+                    pen.setColor(QColor(255,255,255));
+                    painter.setPen(pen);
+                    painter.drawPoint(x,y);
+                }
             }
+            aux++;
         }
+        aux++;
     }
-
-
-
 }
